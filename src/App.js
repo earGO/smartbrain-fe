4ab660e8e58fb103d811a4fbe812d2ad6980e637
earGO <rh.edwin@gmail.com>
2018-09-29 17:27:11 -0500

    background rank and particles added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank'
+import Particles from 'react-particles-js';
 import './App.css';
+
+const paticlesOptions = {
+                          particles: {
+                            number: {
+                              value: 123,
+                              density: {
+                                enable: true,
+                                value_area: 520
+                              }
+                            },
+                          }
+                        }
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+              params={paticlesOptions}
+
+            />
+        
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* 
         
