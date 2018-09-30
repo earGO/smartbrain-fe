2c288166e24b00b8b9edbaf3c98ee3678db7b97b
earGO <rh.edwin@gmail.com>
2018-09-30 12:57:46 -0500

    input state added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,21 @@ const paticlesOptions = {
 
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('submit button pressed');
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +46,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* 
         
         <Facerecognition /> */}
