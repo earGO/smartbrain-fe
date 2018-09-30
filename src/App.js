5a728f744f73acdb5d8a7264542f37087b9709e5
earGO <rh.edwin@gmail.com>
2018-09-30 15:28:10 -0500

    added comments

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
 
+//the variable to set a background particles parameters
 const paticlesOptions = {
                           particles: {
                             number: {
@@ -22,28 +23,29 @@ const paticlesOptions = {
                           }
                         }
 
-
+//the variable to initiate a clarifai-api in an app
 const app = new Clarifai.App({
-  apiKey: '0ae7cd6cb290496a96bafa0c291479b0'
+  apiKey: '0ae7cd6cb290496a96bafa0c291479b0' //here we should place an API key from clarifai
  });
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
+      input: '', //a state for an image ling form input
+      imageUrl: '', //a state for an image url for a recognition
+      box: {}, //a state for a recognized face bounding box
+      route: 'signin',//a state for keeping track of where user on a page is
     }
   }
-
+//a method for a recognized face location
   calculateFaceLocation =(data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box; //a bounding box coordinates from a clarifai API response
+    const image = document.getElementById('inputImage');//an image for a width and height
     const width = Number(image.width);
     const height = Number(image.height);
     console.log(clarifaiFace);
-
+//the absolute coordinates of a recognized face bounding box, the (0;0) point is at the left top corner of an image
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
@@ -51,28 +53,28 @@ class App extends Component {
       bottomRow: height - clarifaiFace.bottom_row * height,
     }
   }
-
+//a method for displaying recognized face bounding box; it takes a coordinates from calculateFaceLocation method as an input and gives it to a box state
   displayFaceBox =(boxcoord) => {
     console.log(boxcoord);
     this.setState({box: boxcoord});
   }
-
+//a method for a state changing from an image link form input
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
-
+//a method for a Detect button
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, 
-    this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    this.setState({imageUrl: this.state.input}); //takes input state as an url for an image
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, //sets face recognition model
+    this.state.input)//takes actual image from the web
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))//creates a bounding box to send it as a prop to Facerecognition component
     .catch(err => console.log(err));
   }
-
+//main render method for a components
   render() {
     return (
       <div className="App">
-        <Particles className='particles'
+        <Particles className='particles' //creates a background particles, using the particlesOptions variable as parameters
               params={paticlesOptions} 
 
             />
@@ -82,10 +84,13 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}
+          onInputChange={this.onInputChange} //this prop manages the image URL input in a form
+          onButtonSubmit={this.onButtonSubmit}//this prop manages clicking on a Detect-button
         />
-        <Facerecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        <Facerecognition 
+          box={this.state.box} //this prop manages the bounding box for a recognized face on an image
+          imageUrl={this.state.imageUrl}//this prop manages image URL for a rendering purposes - it actualy lods images in a component and shows it on a web-page
+        />
       </div>
     );
   }
--- src/Components/Facerecognition/Facerecognition.js
+++ src/Components/Facerecognition/Facerecognition.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Facerecognition.css';
 
-const Facerecognition = ({imageUrl, box}) => {
+//here we are recieving props from a top APP level
+const Facerecognition = ({imageUrl, box}) => {  
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img id='inputImage' alt='' src={imageUrl} width='700px' height='auto'/>
-                <div className='bounding-box' 
+            {/*here we R loading image from the URL and showing it on a web-page*/}
+                <img id='inputImage' alt='' src={imageUrl} width='700px' height='auto'/> 
+                {/*here we R drawing a bounding box from an image detection*/}
+                <div className='bounding-box'       
                     style={{
                             top: box.topRow,
                             right: box.rightCol,
